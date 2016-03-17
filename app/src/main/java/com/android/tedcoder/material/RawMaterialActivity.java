6a2549825d95ee5427d37d23a5cd31fe8786243f
孔建华08490 <kjh08490@ly.com>
2016-03-17 18:53:11 +0800

    add line item view

--- app/src/main/java/com/android/tedcoder/material/RawMaterialActivity.java
+++ app/src/main/java/com/android/tedcoder/material/RawMaterialActivity.java
@@ -59,11 +59,13 @@ public class RawMaterialActivity extends AppCompatActivity {
 
     private LinearLayout ll_a;
     private LinearLayout ll_b;
+    private LinearLayout ll_c;
     private LinearLayout ll_d;
     private LinearLayout ll_e;
 
     private RawLineView rawLineView_a = null;
     private RawLineView rawLineView_b = null;
+    private RawLineView rawLineView_c = null;
     private RawLineView rawLineView_d = null;
     private RawLineView rawLineView_e = null;
 
@@ -84,6 +86,7 @@ public class RawMaterialActivity extends AppCompatActivity {
 
         ll_a = (LinearLayout) findViewById(R.id.ll_a);
         ll_b = (LinearLayout) findViewById(R.id.ll_b);
+        ll_c = (LinearLayout) findViewById(R.id.ll_c);
         ll_d = (LinearLayout) findViewById(R.id.ll_d);
         ll_e = (LinearLayout) findViewById(R.id.ll_e);
 
@@ -91,12 +94,14 @@ public class RawMaterialActivity extends AppCompatActivity {
 
         rawLineView_a = new RawLineView(this);
         rawLineView_b = new RawLineView(this);
+        rawLineView_c = new RawLineView(this);
         rawLineView_d = new RawLineView(this);
         rawLineView_e = new RawLineView(this);
 
 
         ll_a.addView(rawLineView_a, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         ll_b.addView(rawLineView_b, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ll_c.addView(rawLineView_c, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         ll_d.addView(rawLineView_d, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         ll_e.addView(rawLineView_e, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
@@ -186,6 +191,7 @@ public class RawMaterialActivity extends AppCompatActivity {
     private void handleCellList(ArrayList<Cell> cellList) {
         ArrayList<Cell> cell_a = new ArrayList<Cell>();
         ArrayList<Cell> cell_b = new ArrayList<Cell>();
+        ArrayList<Cell> cell_c = new ArrayList<Cell>();
         ArrayList<Cell> cell_d = new ArrayList<Cell>();
         ArrayList<Cell> cell_e = new ArrayList<Cell>();
 
@@ -195,6 +201,8 @@ public class RawMaterialActivity extends AppCompatActivity {
                 cell_a.add(cell);
             } else if (!TextUtils.isEmpty(cell.LocCode) && cell.LocCode.contains("B")) {
                 cell_b.add(cell);
+            } else if (!TextUtils.isEmpty(cell.LocCode) && cell.LocCode.contains("C")) {
+                cell_c.add(cell);
             } else if (!TextUtils.isEmpty(cell.LocCode) && cell.LocCode.contains("D")) {
                 cell_d.add(cell);
             } else if (!TextUtils.isEmpty(cell.LocCode) && cell.LocCode.contains("E")) {
@@ -204,6 +212,7 @@ public class RawMaterialActivity extends AppCompatActivity {
 
         rawLineView_a.setLineCellData(cell_a);
         rawLineView_b.setLineCellData(cell_b);
+        rawLineView_c.setLineCellData(cell_c);
         rawLineView_d.setLineCellData(cell_d);
         rawLineView_e.setLineCellData(cell_e);
 
