01f5d5c957d2f69a3efb369ce45f369bf981d27b
kjh08490@ly.com <kjh08490@ly.com>
2016-04-08 22:42:59 +0800

    fix ui & save sp about hostname

--- app/src/main/java/com/android/tedcoder/material/MainActivity.java
+++ app/src/main/java/com/android/tedcoder/material/MainActivity.java
@@ -1,6 +1,8 @@
 package com.android.tedcoder.material;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -44,6 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         et_request_time = (EditText) findViewById(R.id.et_request_time);
         et_ainimation_time = (EditText) findViewById(R.id.et_ainimation_time);
 
+        String host = getSP(Host.KWYHOST);
+        if (TextUtils.isEmpty(host)) {
+            et_host.setText(Host.HOST);
+        }else{
+            et_host.setText(host);
+        }
+
         btn_fresco = (Button) findViewById(R.id.btn_fresco);
         btn_fresco.setOnClickListener(this);
 
@@ -75,6 +84,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void savaSP(String key, String value) {
+        //实例化SharedPreferences对象（第一步）
+        SharedPreferences mySharedPreferences = getSharedPreferences("test",
+                Activity.MODE_PRIVATE);
+        //实例化SharedPreferences.Editor对象（第二步）
+        SharedPreferences.Editor editor = mySharedPreferences.edit();
+        //用putString的方法保存数据
+        editor.putString(key, value);
+        //提交当前数据
+        editor.commit();
+    }
+
+    private String getSP(String key) {
+        //同样，在读取SharedPreferences数据前要实例化出一个SharedPreferences对象
+        SharedPreferences sharedPreferences = getSharedPreferences("test",
+                Activity.MODE_PRIVATE);
+        // 使用getString方法获得value，注意第2个参数是value的默认值
+        String value = sharedPreferences.getString(key, "");
+        return value;
+    }
+
     private void initSetting() {
         String host = et_host.getText().toString();
         String request_time = et_request_time.getText().toString();
@@ -82,6 +112,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (!TextUtils.isEmpty(host)) {
             // do  something
             Host.HOST = host;
+            savaSP(Host.KWYHOST, host);
         }
 
         int time = 10;
--- app/src/main/java/com/android/tedcoder/material/api/Host.java
+++ app/src/main/java/com/android/tedcoder/material/api/Host.java
@@ -7,6 +7,8 @@ public class Host {
     public static String HOST = "http://mes.seekersoft.cn/";
 //    public static String HOST = "http://192.168.163.11:8077/";
 
+    public static final String KWYHOST = "keyhost";
+
     // 时隔10秒请求一次
     public static int TENLOOPER = 10;
 
